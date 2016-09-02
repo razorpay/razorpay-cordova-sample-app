c00dd03db80dd7b8d6c918885e9f280f1637828c
selvagsz <selvaganeshbsg@gmail.com>
2016-09-02 17:17:00 +0530

    Remove prev sample

--- README.md
+++
@@ -1,3 +0,0 @@
-# Razorpay cordova sample app
-
-This is a sample app integrating Razorpay cordova plugin. Don't forget to copy plugins folder. And plugin configuration from `config.xml`
--- plugins/com.razorpay.cordova/www/RazorpayCheckout.js
+++
@@ -1,7 +0,0 @@
-/*global cordova, module*/
-
-module.exports = {
-    open: function (options, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Checkout", "open", [JSON.stringify(options)]);
-    }
-};
