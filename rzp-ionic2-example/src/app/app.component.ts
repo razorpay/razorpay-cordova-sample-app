3b04506bd1a6f2661a28975b499f68fa90594381
selvagsz <selvaganeshbsg@gmail.com>
2016-12-12 14:53:57 +0530

    Make the example layout simple

--- rzp-ionic2-example/src/app/app.component.ts
+++ rzp-ionic2-example/src/app/app.component.ts
@@ -2,15 +2,10 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
-import { TabsPage } from '../pages/tabs/tabs';
-
-
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = TabsPage;
-
   constructor(platform: Platform) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
